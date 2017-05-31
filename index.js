b3c93303d56cede5a0bb4b6f0b9e1f1c8cc2961b
Vernon <vernonmu@gmail.com>
2017-05-31 08:08:43 -0500

    adding premier endpoints

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const massive = require('massive')
 const db = require('./db')
 const session = require('express-session')
 const nodemailer = require('nodemailer');
-
 const config = require('./config.js')
+const port = config.port
+const db = require('./db.js')
+require ('dotenv').config()
 
 const app = express()
 
-const port = config.port
 
 app.use(bodyParser());
 app.use(cors({
@@ -82,7 +83,70 @@ let transporter = nodemailer.createTransport({
     }
 });
 
+app.get('/ptcd/cal', (req,res,next) => {
+  axios.get(`https://www.googleapis.com/calendar/v3/calendars/${process.env.calendarid}/events?key=${process.env.mykey}`)
+  .then((response) => {
+      let items = response.data.items.map( (val, idx) => {
+        if (val.start) {
+          val.date = moment(val.start.dateTime).format('dddd, MMMM DD, YYYY')
+          // console.log(val.date);
+        }
+      })
+    res.status(200).json(response.data)
+  })
+  .catch( error => {
+    return console.log(error);
+  })
+})
 
+app.post('/ptcd/', (req,res,next) => {
+  console.log(req.body);
+  console.log(req.body.gender);
+  let date = Date.now()
+
+  db.createAthlete([
+    req.body.athlete_first,
+    req.body.athlete_last,
+    req.body.dob,
+    req.body.gender,
+    req.body.school,
+    req.body.parent_first,
+    req.body.parent_last,
+    req.body.street,
+    req.body.city,
+    req.body.zip,
+    req.body.role,
+    req.body.telephone,
+    req.body.email,
+    req.body.emergency_one_first, req.body.emergency_one_last, req.body.emergency_one_relationship,
+    req.body.emergency_one_phone,
+    req.body.emergency_two_first,
+    req.body.emergency_two_last,
+    req.body.emergency_two_relationship,
+    req.body.emergency_two_phone,
+    req.body.medications,
+    req.body.emergency_choice,
+    date], (err, data) => {
+      if (err) {next(err)}
+      return res.status(200).json(data)
+  })
+})
+
+app.post('/ptcd/message', (req,res,next) => {
+  console.log(req.body);
+  let tempDate = Date.now()
+  let date = moment(tempDate).format('dddd, MMMM DD, YYYY h:mm:ss')
+
+  db.createMessage([
+    req.body.first_name,
+    req.body.last_name,
+    req.body.message,
+    date
+  ], (err, message) => {
+    if (err) {next(err)}
+    else {return res.status(200).json(message)}
+  })
+})
 
 
 
